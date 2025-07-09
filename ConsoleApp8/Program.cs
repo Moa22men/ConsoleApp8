5c3d201f7c8735652261a7aa11734782c160b922
Moa22men <moamenabdelrahman22@gmail.com>
2025-07-09 06:10:14 +0300

    Q1 solved

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -2,11 +2,48 @@
 
 namespace ConsoleApp8
 {
+    #region Q1
+    enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA,
+    }
+    
+
+    class Employee
+    {
+        public int ID;
+        public string Name;
+        public char Gender; 
+        public double Salary;
+        public SecurityLevel Security;
+
+        public Employee(int id, string name, char gender, double salary, SecurityLevel security)
+        {
+            ID = id;
+            Name = name;
+            if (gender == 'M' || gender == 'F')
+                Gender = gender;
+            else
+                Gender = 'M'; 
+
+            Salary = salary;
+            Security = security;
+        }
+
+        public string GetInfo()
+        {
+            return "ID: " + ID + " Name: " + Name + " Gender: " + Gender + " Salary: " + Salary.ToString("C") + " Security Level: " + Security;
+        }
+        #endregion
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
